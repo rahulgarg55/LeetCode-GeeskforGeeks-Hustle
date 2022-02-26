0a6f9965b43eb92f97cf30afc0175eb3309df634
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-02-26 23:34:00 +0530

    Added solution - LeetHub

---
+++ Find position of set bit - GFG/find-position-of-set-bit.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int findPosition(int N) {
+        // code here
+        int pos=1,count=0;
+        while(N>0){
+            if(N&1==1)
+            count++;
+            else pos++;
+        
+        N=N>>1;
+    }
+        if(count==1)return pos;
+        else return -1;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findPosition(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
