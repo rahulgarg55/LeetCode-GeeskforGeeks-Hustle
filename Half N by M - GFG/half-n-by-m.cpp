cf586b657f91563b58947e2bc24c8e57e5f6fd06
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-03-03 05:54:09 -0800

    Added solution - LeetHub

---
+++ Half N by M - GFG/half-n-by-m.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int mthHalf(int N, int M){
+        // code here
+        N=N/pow(2,M-1);
+        return N;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, M;
+        cin>>N>>M;
+        
+        Solution ob;
+        cout<<ob.mthHalf(N, M)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
