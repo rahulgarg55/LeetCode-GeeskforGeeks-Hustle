eb5db4ab70ff0f9fd9e8754f3b84b96a60d16861
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-03-29 06:31:54 -0700

    Added solution - LeetHub

---
+++ Smaller on Left - GFG/smaller-on-left.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> Smallestonleft(int arr[], int n);
+
+
+int main() {
+	
+	int t;
+	
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n;
+	    cin >> n;
+	    int arr[n+1];
+	    
+	    for(int i=0;i<n;i++){
+	        cin>> arr[i];
+	    }
+	    
+	    vector<int> b;
+	    b = Smallestonleft(arr, n);
+	    for(int i=0;i<n;i++)
+	        cout << b[i] << " ";
+	       
+	    cout << endl;
+	    
+	    
+	}
+	
+	return 0;
+}// } Driver Code Ends
+
+
+vector<int> Smallestonleft(int arr[], int n)
+{
+    // Complete the function
+  vector<int>v; //to store resultant elements
+  set<int>s; // The set stores the elements in sorted order.
+//All the elements in a set have unique values.
+//The value of the element cannot be modified once it is added to the set, though it is possible to remove and then add the modified value of that element. Thus, the values are immutable.
+//Sets follow the Binary search tree implementation.
+//The values in a set are unindexed.
+  for(int i=0;i<n;i++){ 
+      auto it=s.lower_bound(arr[i]); //lower bound return pointer(address of the value) of the value just immediate greater than the element we passed in that
+      if(it==s.begin()){ //means set is empty(for eg for 2 in 1st sample test case)
+      v.push_back(-1); //no element present on left side which is strictly smaller than itself
+      }else{
+          it--; //
+          v.push_back(*it);
+  }
+  s.insert(arr[i]); // for eg,2 goes to set in 1st test case (when search goes to 3,it searches in set and 2 is present in set)
+  }
+  return v;
+} 
+   
+    
