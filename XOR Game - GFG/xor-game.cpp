3c23934e8c581211609fe65e24ee3379953bf336
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-04-05 10:56:38 -0700

    Added solution - LeetHub

---
+++ XOR Game - GFG/xor-game.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int xorCal(int k){
+        // code here
+      if(k==1)return 2;
+        if((k&(k+1))==0)return k/2;
+        else return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        cout<<ob.xorCal(k)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
