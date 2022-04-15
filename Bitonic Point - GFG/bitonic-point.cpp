f4dc3bbeaa79faf3208197235f0ffa127f010768
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-04-15 05:02:39 -0700

    Added solution - LeetHub

--- Bitonic Point - GFG/bitonic-point.cpp
+++ Bitonic Point - GFG/bitonic-point.cpp
@@ -10,15 +10,16 @@ class Solution{
 public:
 	
 	int findMaximum(int arr[], int n) {
-	   int l=0,r=n-1,mid;  
-	   while(l<=r){
-	       int mid=l+(r-l)/2;
-	       if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-	       return arr[mid];
-	       else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1])
-	       l=mid+1;
-	       else if(arr[mid]<arr[mid-1])
-	       r=mid-1;
+	    int l=0,r=n-1,mid;
+    while(l<=r)
+    {
+        mid=l+(r-l)/2;
+        if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])
+        return arr[mid];
+        else if(arr[mid]>arr[mid-1]&&arr[mid]<arr[mid+1])
+        l=mid+1;
+        else if(arr[mid]<arr[mid-1])
+        r=mid-1;
 	   
 	        }
 	    
