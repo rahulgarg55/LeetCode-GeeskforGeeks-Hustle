b2eca80b39c9103acc787c69a3d59cb0bd553a2c
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-04-20 11:44:37 +0530

    Added solution - LeetHub

---
+++ Remove common characters and concatenate - GFG/remove-common-characters-and-concatenate.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+// C++ program Find concatenated string with 
+// uncommon characters of given strings 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to remove common characters and concatenate two strings.
+    string concatenatedString(string s1, string s2) 
+    { 
+        // Your code here
+       string ans="";
+       for(auto i:s1){
+           if(s2.find(i)==string::npos)
+           ans+=i;
+       }
+       for(auto i:s2){
+           if(s1.find(i)==string::npos)
+               ans+=i;
+           
+       }
+return(ans=="")?"-1":ans;
+}
+};
+
+// { Driver Code Starts.
+
+/* Driver program to test above function */
+int main() 
+{ 
+	int t;
+	cin >> t;
+	
+	while(t--){
+    	string s1, s2;
+    	cin >> s1 >> s2;
+    	Solution obj;
+    	string res = obj.concatenatedString(s1, s2);
+    	cout<<res<<endl;
+	}
+	return 0; 
+}
+  // } Driver Code Ends
