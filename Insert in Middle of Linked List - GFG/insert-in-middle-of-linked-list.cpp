a7c0ab653628db638d979ddb1ec47978fe8b0b53
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-05-07 10:41:49 -0700

    Added solution - LeetHub

---
+++ Insert in Middle of Linked List - GFG/insert-in-middle-of-linked-list.cpp
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+// function to display the linked list
+void display(Node* head)
+{
+	while (head != NULL) {
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+Node* insertInMiddle(Node* head, int x);
+
+int main()
+{
+    int T, n, x;
+    cin>>T;
+    while(T--)
+    {
+        cin>> n >> x;
+        
+        struct Node *head = new Node(x);
+        struct Node *tail = head;
+        
+        for (int i=0; i<n-1; i++)
+        {
+            cin>>x;
+            tail->next = new Node(x);
+            tail = tail->next;
+        }
+        
+        cin>> x;
+        head = insertInMiddle(head, x);
+        display(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to insert a node in the middle of the linked list.
+Node* insertInMiddle(Node* head, int x)
+{
+	// Code here
+
+	Node *slow=head;
+	Node *fast=head->next;
+	while(fast!=NULL && fast->next!=NULL){
+	   slow=slow->next;
+	   fast=fast->next->next;
+	}
+	  Node *temp=new Node(x);
+	  temp->next=slow->next;
+	  slow->next=temp;
+	  return head;
+	  
+	}
+	
