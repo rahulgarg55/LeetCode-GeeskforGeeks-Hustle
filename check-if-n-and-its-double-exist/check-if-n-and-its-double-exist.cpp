544b9758090e5ace7c5f85fb0475fd7b857339d5
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-06-10 23:02:24 -0700

    Time: 9 ms, Memory: 9.9 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        int n=arr.size();
-     bool ans=false;
-        for(int i=0;i<n;i++){
-                    
-            for(int j=0;j<n;j++){
-                if(i!=j)
-            if(arr[i]==(arr[j]*2))
-            ans=true;
-                }
-        }
-                return ans;
+ int n= arr.size();
+     
+sort (arr.begin(),arr.end());
+for(int i=0;i<n;i++){
+int s=0;
+int e = n-1;
+int mid = s + (e-s)/2;
+int target = arr[i]*2;
+while(s<=e){
+
+           if (arr[mid]== target && mid!=i)
+               return true;
+           else if (arr[mid]< target)
+               s= mid +1;
+           else 
+               e = mid -1;
+          mid=s+(e-s)/2;
+       }
+       
+       
+   }
+    return false;
+
             
                 
      
