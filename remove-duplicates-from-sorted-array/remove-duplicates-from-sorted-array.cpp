09eefa4f058d02f18755411542849e73fe15289f
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-06-13 07:23:31 -0700

    Time: 11 ms, Memory: 18.5 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       int n=nums.size();
+        int n=nums.size();
+        int j=0;
         if(n==0||n==1)return n;
-        int j=0; //to store index of next unique element.
         for(int i=0;i<n-1;i++){
-            if(nums[i]!=nums[i+1])
+            if(nums[i]!=nums[i+1]){
                 nums[j++]=nums[i];
+            }
         }
-            nums[j++]=nums[n-1]; //whether last element is unique or repeated we haven't stored it previously 
-        
+        nums[j++]=nums[n-1];
         return j;
     }
 };
