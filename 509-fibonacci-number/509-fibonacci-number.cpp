7e11dc93bdcbef40f3edc2a6a6750ab5684d62d7
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-07-06 09:28:51 -0700

    Time: 2 ms (64.32%), Space: 5.8 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+    int a = 0, b = 1, c, i;
+    if( n == 0)
+        return a;
+    for(i = 2; i <= n; i++)
+    {
+       c = a + b;
+       a = b;
+       b = c;
+    }
+    return b;
+        
+    }
+};
