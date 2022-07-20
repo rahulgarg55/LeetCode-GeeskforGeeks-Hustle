fde577b1d5f88cbba6e5854021be793c55550ff7
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-07-20 06:23:56 -0700

    Time: 313 ms (62.96%), Space: 37.9 MB (87.39%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-        int total =0,k=s.size();
-        unordered_map<string,int>stringmap;
+      int total=0,k=s.size();
+        unordered_map<string,int>m;
         for(int i=0;i<words.size();i++){
-            stringmap[words[i]]++;
+            m[words[i]]++;
         }
-        for(auto st: stringmap){
-           int index1=0,index2=0;
+        for(auto st:m){
+            int index1=0,index2=0;
             string m=st.first;
             int p=m.size();
-            while(index1<k&&index2<p){
-                if(s[index1]==m[index2]){
+            while(index1<k && index2<p){
+                if(s[index1]==m[index2]){ //abcde //acd
+                    //index1 is at a //index2 is at a of acd
                     index1++;
                     index2++;
-                }
-                else{
-                    index1++;
+                }else{
+                    index1++; //index1 is at b of abcde and index 2 is at c of acd
                 }
             }
             if(index2==p){
-                total+=st.second;
+                total+=st.second; //index2 will be equal to size of "acd"
             }
         }
         return total;
