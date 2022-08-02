fcf74019e8314a0cf7f7783e16439f50a1f09926
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-08-01 23:35:58 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (75.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -4,13 +4,11 @@ public:
         int dp[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || j==0)
-                    dp[i][j]=1;
-        else
-      dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                
+                if(i==0||j==0)dp[i][j]=1;
+                else
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-        return dp[m-1][n-1]; 
+        return dp[m-1][n-1];
     }
 };
