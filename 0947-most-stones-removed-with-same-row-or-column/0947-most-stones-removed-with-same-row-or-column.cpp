5283b6b5e5d376a41f9176f9f6d7ff50af00690d
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-11-14 22:04:10 +0530

    Time: 381 ms (42.80%), Space: 18.7 MB (41.39%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  void dfs(const vector<vector<int>>& graph,int u,unordered_set<int>&seen){
+      for(const int v:graph[u])
+          if(seen.insert(v).second)
+              dfs(graph,v,seen);
+  }
+    int removeStones(vector<vector<int>>& stones) {
+          int ans=0;
+    vector<vector<int>>graph(stones.size());
+    unordered_set<int>seen;
+    for(int i=0;i<stones.size();i++)
+        for(int j=i+1;j<stones.size();j++)
+            if(stones[i][0]==stones[j][0]||stones[i][1]==stones[j][1]){
+                graph[i].push_back(j);
+    graph[j].push_back(i);
+    }
+    for(int i=0;i<stones.size();i++)
+        if(seen.insert(i).second){
+            dfs(graph,i,seen);
+            ++ans;
+        }
+    return stones.size()-ans;
+    } 
+};
