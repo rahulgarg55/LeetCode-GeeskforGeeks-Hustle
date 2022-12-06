9999f7a66f7f50e4b0b0278d649d875371c5be70
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2022-12-06 22:59:57 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -10,38 +10,40 @@ class Solution
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
 	    // Code here
-	    int m=grid.size();
-	    int n=grid[0].size();
-	    vector<vector<int>>ans(m,vector<int>(n,0));
-	    vector<vector<int>>vis(m,vector<int>(n,0));
-	    queue<pair<pair<int,int>,int>>q; //i,j,dist
-	    for(int i=0;i<m;i++){
-	        for(int j=0;j<n;j++){
-	            if(grid[i][j]==1){
-	                q.push({{i,j},0});
-	                vis[i][j]=1;
-	            }
-	        }
-	    }
-	    int dx[]={1,-1,0,0};
-	    int dy[]={0,0,1,-1};
-	    while(!q.empty()){
-	        int x=q.front().first.first;
-	        int y=q.front().first.second;
-	        int dist=q.front().second;
-	        ans[x][y]=dist;
-	        q.pop();
-	        for(int k=0;k<4;k++){
-	            int nx=x+dx[k],ny=y+dy[k];
-	            if(nx>=0 && nx<m &&ny>=0 &&ny<n &&!vis[nx][ny]){
-	                q.push({{nx,ny},dist+1});
-	                vis[nx][ny]=1;
-	            }
-	        }
-	    }
-	    return ans;
-	    
-	}
+	 int n=grid.size();
+int m=grid[0].size();
+vector<vector<int>>vis(n,vector<int>(m,0));
+vector<vector<int>>ans(n,vector<int>(m,0));
+queue<pair<pair<int,int>,int>>q;
+for(int i=0;i<n;i++){
+for(int j=0;j<m;j++){
+if(grid[i][j]==1){
+q.push({{i,j},0});
+vis[i][j]=1;
+}else{
+    vis[i][j]=0;
+}
+}
+}
+int dx[]={-1,1,0,0};
+int dy[]={0,0,1,-1};
+while(!q.empty()){
+int row=q.front().first.first;
+int col=q.front().first.second;
+int steps=q.front().second;
+q.pop();
+ans[row][col]=steps;
+for(int i=0;i<4;i++){
+int nrow=row+dx[i];
+int ncol=col+dy[i];
+if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and vis[nrow][ncol]==0){
+vis[nrow][ncol]=1;
+q.push({{nrow,ncol},steps+1});
+}
+}
+}
+return ans;
+}
 	
 };
 
