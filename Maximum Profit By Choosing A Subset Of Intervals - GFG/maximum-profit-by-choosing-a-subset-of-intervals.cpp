12cfb73e727c429b5abf3506744b36119aa237fc
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2023-01-04 22:03:13 +0530

    Added solution - LeetHub

--- Maximum Profit By Choosing A Subset Of Intervals - GFG/maximum-profit-by-choosing-a-subset-of-intervals.cpp
+++ Maximum Profit By Choosing A Subset Of Intervals - GFG/maximum-profit-by-choosing-a-subset-of-intervals.cpp
@@ -9,33 +9,34 @@ using namespace std;
 class Solution {
   public:
   static bool compare(vector<int>&a,vector<int>&b){
-      if(a[0]==b[0] and a[1]==b[1])
-      return a[2]>b[2];
-      else if(a[1]==b[1])
-      return a[0]<b[0];
-      else if(a[0]==b[0])
-      return a[1]<b[1];
+      if(a[0]==b[0] and a[1]==b[1])return a[2]>b[2];
+      else if(a[1]==b[1])return a[0]<b[0];
+      else if(a[0]==b[0])return a[1]<b[1];
       else return a[0]<=b[0];
+      
   }
     int maximum_profit(int n, vector<vector<int>> &intervals) {
         // Write your code here.
-    sort(intervals.begin(),intervals.end(),compare);
-    vector<int>dp(n,0);
-    for(int i=0;i<n;i++)dp[i]=intervals[i][2];
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(intervals[j][0]<intervals[i][0]and
-            intervals[j][1]<=intervals[i][0]){
-                dp[i]=max(dp[i],intervals[i][2]+dp[j]);
+         sort(intervals.begin(),intervals.end(),compare);
+        vector<int>dp(n,0);
+        for(int i=0;i<n;i++)dp[i]=intervals[i][2];
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(intervals[j][0]<intervals[i][0] and
+                intervals[j][1]<=intervals[i][0]){
+                    dp[i]=max(dp[i],intervals[i][2]+dp[j]);
+                }
             }
         }
-    }
-    int max_profit=0;
-    for(int i=0;i<n;i++){
-        max_profit=max(max_profit,dp[i]);
+        int max_profit=0;
+        for(int i=0;i<n;i++){
+            max_profit=max(max_profit,dp[i]);
+        }
+        return max_profit;
         
-    }
-    return max_profit;
+        
+        
     }
 };
 
