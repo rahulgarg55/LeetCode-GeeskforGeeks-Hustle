c0d636e047be45fadaa53274b3036226d8ec1dcf
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2023-01-24 12:52:10 +0530

    Added solution - LeetHub

---
+++ Convert an array to reduced form - GFG/convert-an-array-to-reduced-form.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Converts arr[0..n-1] to reduced form.
+	void convert(int arr[], int n){
+     vector<pair<int,int>>v;
+     for(int i=0;i<n;i++){
+         v.push_back({arr[i],i});
+     }
+     sort(v.begin(),v.end());
+     int temp=0;
+     for(auto i:v){
+         arr[i.second]=temp++;
+     }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.convert(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
