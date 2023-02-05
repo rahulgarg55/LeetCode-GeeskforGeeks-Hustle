7a7ec1d847b5b81cbec34f2c54bcdeb2d24d2653
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2023-02-05 23:01:50 +0530

    Added solution - LeetHub

---
+++ Intersection Point in Y Shaped Linked Lists - GFG/intersection-point-in-y-shaped-linked-lists.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    if (size == 0) return NULL;
+
+    int val;
+    cin >> val;
+
+    Node *head = new Node(val);
+    Node *tail = head;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        cin >> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+
+// } Driver Code Ends
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution
+{
+public:
+    int intersectPoint(Node* head1, Node* head2)
+    {
+        // Your Code Here
+        Node* a=head1,*b=head2;
+        while(a!=b){
+        if(a==NULL)a=head2;
+        a=a->next;
+        if(b==NULL)b=head1;
+     b=b->next;
+    }
+    return a->data;
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+
+/* Driver program to test above function*/
+int main()
+{
+    int T, n1, n2, n3;
+
+    cin >> T;
+    while (T--)
+    {
+        cin >> n1 >> n2 >> n3;
+
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+
+        Node* temp = head1;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL) temp->next = common;
+
+        temp = head2;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL) temp->next = common;
+        
+        Solution ob;
+        cout << ob.intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
