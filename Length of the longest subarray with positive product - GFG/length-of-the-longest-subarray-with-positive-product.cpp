5e0db99b18babd143598567097a90c22bdcb60ff
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2023-02-08 11:22:29 +0530

    Added solution - LeetHub

--- Length of the longest subarray with positive product - GFG/length-of-the-longest-subarray-with-positive-product.cpp
+++ Length of the longest subarray with positive product - GFG/length-of-the-longest-subarray-with-positive-product.cpp
@@ -13,66 +13,25 @@ using namespace std;
 class Solution {
     public:
         int maxLength(vector<int> &arr,int N){
-             int Pos = 0;
- 
-    // Stores the length of current
-    // subarray with negative product
-    int Neg = 0;
- 
-    // Stores the length of the longest
-    // subarray with positive product
-    int res = 0;
- 
-    for (int i = 0; i < N; i++) {
- 
-        if (arr[i] == 0) {
- 
-            // Reset the value
-            Pos = Neg = 0;
-        }
- 
-        // If current element is positive
-        else if (arr[i] > 0) {
- 
-            // Increment the length of
-            // subarray with positive product
-            Pos += 1;
- 
-            // If at least one element is
-            // present in the subarray with
-            // negative product
-            if (Neg != 0) {
- 
-                Neg += 1;
-            }
- 
-            // Update res
-            res = max(res, Pos);
-        }
- 
-        // If current element is negative
-        else {
- 
-            swap(Pos, Neg);
- 
-            // Increment the length of subarray
-            // with negative product
-            Neg += 1;
- 
-            // If at least one element is present
-            // in the subarray with positive product
-            if (Pos != 0) {
- 
-                Pos += 1;
-            }
- 
-            // Update res
-            res = max(res, Pos);
-        }
-    }
-    return res;
+           int ans=0,freqneg=-1,pzero=-1,neg=0;
+           for(int i=0;i<N;i++){
+                if(arr[i]<0){
+                   neg++;
+                   if(freqneg==-1)freqneg=i;
+                }
+                if(arr[i]==0){
+                    neg=0,pzero=i,freqneg=-1;
+                }else{
+                    if(neg%2==0){
+                        ans=max(ans,i-pzero);
+                    }else{
+                        ans=max(ans,i-freqneg);
+                    }
+                }
            
-        }
+               }
+               return ans;
+           }
 };
 
 //{ Driver Code Starts.
