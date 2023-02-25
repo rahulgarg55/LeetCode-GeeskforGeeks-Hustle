b349a030b6cc1a0373ff4574a4ce5d564c7294f2
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2023-02-25 16:04:35 +0530

    Added solution - LeetHub

--- Valid Compressed String - GFG/valid-compressed-string.cpp
+++ Valid Compressed String - GFG/valid-compressed-string.cpp
@@ -9,29 +9,25 @@ using namespace std;
 class Solution {
   public:
     int checkCompressed(string s, string t) {
-      int i=0,j=0;
-     
-      while(j<t.size() and i<s.size()){
-          if(t[j]==s[i]){
-              i++;
-          j++;
-          continue;
-          }
-          else{
-              int c=0;
-              while(j<t.size() and isdigit(t[j])){
-                
-                  c=c*10+(t[j]-'0');
-                 j++;
-              }
-          
-          i+=c;
-      }
-      if(s[i]!=t[j])return 0;
-      }
-          if(i==s.size() )return 1;
-          else return 0;
-    
+        // code here
+        int i=0,j=0;
+        while(j<t.size() and i<s.size()){
+            if(t[j]==s[i]){
+                i++;
+                j++;
+                continue;
+            }else{
+                int c=0;
+                while(j<t.size() and isdigit(t[j])){
+                    c=c*10+(t[j]-'0');
+                    j++;
+                }
+                i+=c;
+            }
+            if(s[i]!=t[j])return 0;
+        }
+        if(i==s.size())return 1;
+        else return 0;
     }
 };
 
