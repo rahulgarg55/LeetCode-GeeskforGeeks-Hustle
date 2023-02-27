f028a36cddd10d4112b872d8e242a245053e6f39
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2023-02-27 23:24:05 +0530

    Added solution - LeetHub

---
+++ Reverse both parts - GFG/reverse-both-parts.cpp
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+struct Node *inputList()
+{
+    int n; // length of link list
+    scanf("%d ", &n);
+
+    int data;
+    cin >> data;
+    struct Node *head = new Node(data);
+    struct Node *tail = head;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        cin >> data;
+        tail->next = new Node(data);
+        tail = tail->next;
+    }
+    return head;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+Definition for singly Link List Node
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+
+class Solution
+{
+public:
+   
+    Node *reverse(Node *head, int k)
+    {
+        // code here
+        Node* curr=head;
+        Node* prev=NULL;
+        int c=0;
+        while(c<k and curr!=NULL){
+        Node* temp=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=temp;
+        c++;
+        } //2->1
+        Node* newcurr=curr;
+        Node*prev1=NULL;
+        while(newcurr!=NULL){
+            Node* temp1=newcurr->next;
+            newcurr->next=prev1;
+            prev1=newcurr;
+            newcurr=temp1;
+        } //5->4->3
+        head->next=prev1;
+        return prev;
+        
+        
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        struct Node *head = inputList();
+        int k;
+        cin >> k;
+
+        Solution obj;
+        Node *res = obj.reverse(head, k);
+
+        printList(res);
+    }
+}
+// } Driver Code Ends
